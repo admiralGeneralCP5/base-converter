00ecdcce5a4a56bda48e3cea112a081b62d34e0b
admiralGeneralCP5 <curtispenfold5@gmail.com>
2023-10-26 17:18:52 -0400

    Conversions from base 10 working

--- functions.py
+++ functions.py
@@ -1,16 +1,14 @@
-def two_to_ten(b2num):
+def mult_to_ten(num, base):  # converts a number into base 10
     revnum = []
-    for i in b2num:  # reverses the order so that the power for each place matches the index
+    for i in num:  # reverses the order so that the power for each place matches the index
         revnum.insert(0, int(i))
-
-    print("revnum", revnum)
 
     values_for_addition = []
 
     index_counter = 0
     for i in revnum:
         if i == 1:
-            value = 2**index_counter
+            value = base**index_counter
             values_for_addition.append(value)
         index_counter += 1
 
@@ -20,3 +18,19 @@ def two_to_ten(b2num):
         final += value
 
     return final
+
+
+def ten_to_mult(num, base):  # converts from base 10
+    number = int(num)
+    remainders = []
+    div = 1
+    while div > 0:
+        div = number // base
+        mod = number % base
+        remainders.insert(0, mod)
+
+        number = div
+    result = ""
+    for i in remainders:  # turns the list into a string
+        result += str(i)
+    return result
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from functions import *
 
-user = input("Enter a base 2 number: ")
+while True:
+    number = input("Enter a number: ")
+    base = int(input("What is the base of your number? >>> "))
+    final_base = int(input("What base do you want to convert to? >>> "))
 
-print(two_to_ten(user))
+    # if base == '2':
+    #     num = input("Enter a base 2 number: ")
+    #     print(two_to_ten(num))
 
+    if base == 16:
+        print("base 16")
+    elif base != 10:
+        num_in_10 = mult_to_ten(number, base)
+        if final_base == 10:
+            print(num_in_10)
+        #else:
+            #ten_to_mult(num_in_10, final_base)
+    else:
+        print(ten_to_mult(number, final_base))
+
+
+
+
+# if the base != 10: convert to ten
+# then convert to another base if necessary
+
+# else: determine wht base it will need to be converted to
+
+# add subscripts to output
+
+# add a verbose option to show work
